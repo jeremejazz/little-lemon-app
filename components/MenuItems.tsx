bcc6a6e590554a1bcf38a404322ffa0999893ae1
Jereme Causing <jeremejazz15@gmail.com>
2026-01-28 13:48:36 +0800

    apply flatmenu

--- components/MenuItems.tsx
+++ components/MenuItems.tsx
@@ -1,29 +1,79 @@
-import React from 'react';
+import React from "react";
 
-import { View, Text, ScrollView } from 'react-native';
+import { View, Text, ScrollView, FlatList, StyleSheet } from "react-native";
 
 const menuItemsToDisplay = [
-  'Hummus \n Moutabal \n Falafel \n Marinated Olives \n Kofta \n Eggplant Salad \n Lentil Burger \n Smoked Salmon \n Kofta Burger \n Turkish Kebab \n Fries \n Buttered Rice \n Bread Sticks \n Pita Pocket \n Lentil Soup \n Greek Salad \n Rice Pilaf \n Baklava \n Tartufo \n Tiramisu \n Panna Cotta',
+  { name: "Hummus1", price: "$5.00", id: "1A" },
+  { name: "Moutabal", price: "$5.00", id: "2B" },
+  { name: "Falafel", price: "$7.50", id: "3C" },
+  { name: "Marinated Olives", price: "$5.00", id: "4D" },
+  { name: "Kofta", price: "$5.00", id: "5E" },
+  { name: "Eggplant Salad", price: "$8.50", id: "6F" },
+  { name: "Lentil Burger", price: "$10.00", id: "7G" },
+  { name: "Smoked Salmon", price: "$14.00", id: "8H" },
+  { name: "Kofta Burger", price: "$11.00", id: "9I" },
+  { name: "Turkish Kebab", price: "$15.50", id: "10J" },
+  { name: "Fries", price: "$3.00", id: "11K" },
+  { name: "Buttered Rice", price: "$3.00", id: "12L" },
+  { name: "Bread Sticks", price: "$3.00", id: "13M" },
+  { name: "Pita Pocket", price: "$3.00", id: "14N" },
+  { name: "Lentil Soup", price: "$3.75", id: "15O" },
+  { name: "Greek Salad", price: "$6.00", id: "16Q" },
+  { name: "Rice Pilaf", price: "$4.00", id: "17R" },
+  { name: "Baklava", price: "$3.00", id: "18S" },
+  { name: "Tartufo", price: "$3.00", id: "19T" },
+  { name: "Tiramisu", price: "$5.00", id: "20U" },
+  { name: "Panna Cotta", price: "$5.00", id: "21V" },
 ];
 
-const MenuItems = () => {
+const Item = (item: { name: string; price: string; id?: string }) => {
   return (
-    <View style={{ flex: 0.75 }}>
-      <ScrollView
-        style={{
-          paddingHorizontal: 40,
-          paddingVertical: 40,
-          backgroundColor: "black",
-        }}>
-        <Text style={{ color: 'white', fontSize: 40, flexWrap: 'wrap' }}>
-          View Menu
-        </Text>
-        <Text style={{ color: '#F4CE14', fontSize: 36 }}>
-          {menuItemsToDisplay[0]}
-        </Text>
-      </ScrollView>
+    <View style={itemStyles.container}>
+      <View style={[itemStyles.common, itemStyles.nameColumnContainer]}>
+        <Text style={itemStyles.textStyle}>{item.name}</Text>
+      </View>
+      <View style={[itemStyles.common, itemStyles.priceColumnContainer]}>
+        <Text style={[itemStyles.textStyle, itemStyles.priceColumnText]}>{item.price}</Text>
+      </View>
     </View>
   );
 };
+
+const MenuItems = () => {
+  return (
+    <View style={{ flex: 1 }}>
+      <FlatList
+        data={menuItemsToDisplay}
+        renderItem={({ item }) => <Item name={item.name} price={item.price} />}
+        keyExtractor={(item) => item.id}
+      />
+    </View>
+  );
+};
+
+const itemStyles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: "row",
+  },
+  common:{
+      padding: 20
+  },
+  textStyle:{
+      color: "#F4CE14",
+      fontSize: 20
+  },
+  nameColumnContainer: {
+      flex: 0.75
+  },
+  priceColumnContainer:{
+      flex: 0.25,
+      paddingRight: 20
+      
+  },
+  priceColumnText:{
+    textAlign: 'right',
+  }
+});
 
 export default MenuItems;
